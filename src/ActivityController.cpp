24cb7f99b960950310ec20ed690760c414e60c14
SalmaElBidani <49161951+SalmaElBidani@users.noreply.github.com>
2023-02-25 16:46:32 +0100

    Add files via upload
    fourth commit

---
+++ src/ActivityController.cpp
@@ -0,0 +1,17 @@
+//
+// Created by 164291 on 25/02/2023.
+//
+
+#include "ActivityController.h"
+
+void ActivityController::add(SubActivity s){
+    activity->addSubActivity(s);
+}
+
+void ActivityController::remove(SubActivity s){
+    activity->removeSubActivity(s);
+}
+
+void ActivityController::setActivity(std::shared_ptr<Activity> a){
+    this->activity = a;
+}
---
+++ src/ActivityController.h
@@ -0,0 +1,30 @@
+//
+// Created by 164291 on 25/02/2023.
+//
+
+#ifndef AGENDAPROGRAMMAZIONE_MASTER_ACTIVITYCONTROLLER_H
+#define AGENDAPROGRAMMAZIONE_MASTER_ACTIVITYCONTROLLER_H
+
+#include<memory>
+
+#include "Activity.h"
+#include "SubActivity.h"
+
+
+class ActivityController {
+
+    public:
+        ActivityController(std::shared_ptr<Activity> al) : activity(al) { };
+        ~ActivityController() { };
+
+        void add(SubActivity s);
+        void remove(SubActivity s);
+        void setActivity(std::shared_ptr<Activity> a);
+
+    private:
+        std::shared_ptr<Activity> activity;
+};
+
+
+
+#endif //AGENDAPROGRAMMAZIONE_MASTER_ACTIVITYCONTROLLER_H
