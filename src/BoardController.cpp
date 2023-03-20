b5de799df764ed6817a6d7d4f64f7ffc4e6e4e70
SalmaElBidani <selma.elbidani@stud.unifi.it>
2023-03-20 09:41:27 +0100

    Fixed some bugs in date and update other classes

--- src/BoardController.cpp
+++ src/BoardController.cpp
@@ -1,5 +1,19 @@
 //
-// Created by miriam on 11/03/23.
+// Created by 164291 on 11/03/2023.
 //
 
+#include<iostream>
+
 #include "BoardController.h"
+
+void BoardController::addActivity(std::shared_ptr<Activity> a){
+    board->addActivity(a);
+}
+
+void BoardController::removeActivity(std::string aName){
+    board->removeActivity(aName);
+}
+
+void BoardController::onClose(){
+    board->save();
+}
--- src/BoardController.h
+++ src/BoardController.h
@@ -1,14 +1,27 @@
 //
-// Created by miriam on 11/03/23.
+// Created by 164291 on 11/03/2023.
 //
 
-#ifndef AGENDAPROGRAMMAZIONE_BOARDCONTROLLER_H
-#define AGENDAPROGRAMMAZIONE_BOARDCONTROLLER_H
+#ifndef AGENDAPROGRAMMAZIONE_MASTER_BOARDCONTROLLER_H
+#define AGENDAPROGRAMMAZIONE_MASTER_BOARDCONTROLLER_H
 
+#include<memory>
+#include "Activityt.h"
+#include "Board.h"
 
 class BoardController {
+public:
+    BoardController(std::shared_ptr<Board> b) : board(b) { };
+    ~BoardController() { }
+
+    void addActivity(std::shared_ptr<Activity> a);
+    void removeActivity(std::string a);
+    void onClose();
+
+private:
+    std::shared_ptr<Board> board;
 
 };
 
 
-#endif //AGENDAPROGRAMMAZIONE_BOARDCONTROLLER_H
+#endif //AGENDAPROGRAMMAZIONE_MASTER_BOARDCONTROLLER_H
